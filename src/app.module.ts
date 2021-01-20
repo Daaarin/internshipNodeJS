3966dbc22710c165268f3c993814ee2ef0fce1c5
Asmolve <Asmolve@users.noreply.github.com>
2021-01-20 17:51:15 +0300

    Project created

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ApiController } from './api/api.controller';
+
+@Module({
+  imports: [],
+  controllers: [AppController, ApiController],
+  providers: [AppService],
+})
+export class AppModule {}
