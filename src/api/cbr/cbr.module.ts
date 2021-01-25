08683ed3424b781646c654b005f148ec5a39d3a8
Asmolve <Asmolve@users.noreply.github.com>
2021-01-25 21:35:16 +0300

    It's not working properly

--- src/api/cbr/cbr.module.ts
+++ src/api/cbr/cbr.module.ts
@@ -10,12 +10,12 @@ import { IResponse } from '../api.module'
 export class CbrModule {
   public readonly url = 'http://www.cbr.ru/DailyInfoWebServ/DailyInfo.asmx'
   public readonly wsdl = this.url + '?WSDL'
-  public readonly xml = `<?xml version="1.0" encoding="utf-8"?>
+  /* public readonly xml = `<?xml version="1.0" encoding="utf-8"?>
       <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
       <soap12:Body>
         <MainInfoXML xmlns="http://web.cbr.ru/" />
       </soap12:Body>
-      </soap12:Envelope>`
+      </soap12:Envelope>`*/
   public SoapClient
   public response = new response()
 }
