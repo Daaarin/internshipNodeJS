8e51ac34c4330cf6645813294e0c8e22260ba419
Asmolve <Asmolve@users.noreply.github.com>
2021-01-29 14:47:47 +0300

    Input date validation is done

--- src/api/cbr/cbr.service.ts
+++ src/api/cbr/cbr.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { response, dateYMD } from './cbr.module'
 import soap = require('soap')
+import moment = require('moment')
 import { IResponse } from '../api.module'
 
 @Injectable()
@@ -8,7 +9,80 @@ export class CbrService {
   static wsdl = 'http://www.cbr.ru/DailyInfoWebServ/DailyInfo.asmx?WSDL'
   async getRate(inputDate): Promise<IResponse> {
     const outputResponse = new response()
-    let date = new dateYMD(inputDate)
+    /*console.log(moment(inputDate).format('YYYY-MM-DD'))
+    console.log(moment(inputDate).format('DD-MM-YYYY'))
+    console.log(moment(inputDate).format('YYYY-DD-MM'))
+    console.log(moment(inputDate).format('MM-DD-YYYY'))*/
+    /*switch (true) {
+      case moment(inputDate).isValid():
+        inputDate = moment(inputDate).format('YYYY-MM-DD')
+        console.log('#1 taken:', inputDate)
+        break
+      case moment(inputDate, ['YYYY-DD-MM', 'MM-DD-YYYY']).isValid():
+        console.log('#2 taken', inputDate)
+        outputResponse.message =
+          "Неправильный вид даты - должен быть вида 'ГГГГ-ММ-ДД', например, '2017-01-15'"
+        outputResponse.statusCode = 400
+        console.log(outputResponse)
+        return outputResponse
+      default: {
+        outputResponse.message =
+          "Неправильный вид даты - должен быть вида 'ГГГГ-ММ-ДД', например, '2017-01-15'"
+        outputResponse.statusCode = 400
+        console.log('Date: ', moment(inputDate))
+        console.log(outputResponse)
+        return outputResponse
+      }
+    }*/
+    if (moment(inputDate).isValid()) {
+      console.log(
+        `Date (${inputDate}) is valid: ${moment(inputDate)} ${moment(
+          inputDate,
+        ).isValid()}`,
+      )
+      inputDate = moment(inputDate).format('YYYY-MM-DD')
+      console.log('Format: YYYY-MM-DD', inputDate)
+    } else if (moment(inputDate, 'MM-DD-YYYY').isValid()) {
+      inputDate = moment(inputDate, 'MM-DD-YYYY').format('YYYY-MM-DD')
+      console.log('Format: MM-DD-YYYY', inputDate)
+      //inputDate = moment(inputDate).format("YYYY-MM-DD");
+    } else if (moment(inputDate, 'YYYY-DD-MM').isValid()) {
+      inputDate = moment(inputDate, 'YYYY-DD-MM').format('YYYY-MM-DD')
+      console.log('Format: YYYY-DD-MM', inputDate)
+    } else if (moment(inputDate, 'DD-MM-YYYY').isValid()) {
+      inputDate = moment(inputDate, 'DD-MM-YYYY').format('YYYY-MM-DD')
+      console.log('Format: DD-MM-YYYY', inputDate)
+    } else {
+      console.log('Date: ', moment(inputDate))
+      outputResponse.message =
+        "Неправильный вид даты - должен быть в формате ISO или вида 'ГГГГ-ММ-ДД' либо 'ДД-ММ-ГГГГ', например, '2017-01-15' или '25-03-2015'"
+      outputResponse.statusCode = 400
+      return outputResponse
+    }
+    console.log(moment(inputDate))
+    /*if (moment(inputDate).isValid()) {
+      inputDate = moment(inputDate).format('YYYY-MM-DD')
+      console.log('#1 taken:', inputDate)
+    } else if (moment(inputDate, ['YYYY-DD-MM', 'MM-DD-YYYY']).isValid()) {
+      console.log('#2 taken', inputDate)
+      outputResponse.message =
+        "Неправильный вид даты - должен быть вида 'ГГГГ-ММ-ДД' либо 'ММ-ДД-ГГГГ', например, '2017-01-15' или '03-25-2015'"
+      outputResponse.statusCode = 400
+      console.log(outputResponse)
+      return outputResponse
+    } else if (moment(inputDate, 'DD-MM-YYYY').isValid()) {
+      console.log('#3 taken', inputDate)
+      inputDate = moment(inputDate, "DD-MM-YYYY")
+      inputDate = moment(inputDate).format("YYYY-DD-MM")
+    } else {
+      outputResponse.message =
+        "Неправильный вид даты - должен быть вида 'ГГГГ-ММ-ДД', например, '2017-01-15'"
+      outputResponse.statusCode = 400
+      console.log('Date: ', moment(inputDate))
+      console.log(outputResponse)
+      return outputResponse
+    }*/
+    /*let date = new dateYMD(inputDate)
     const dateToday = dateYMD.dateNow()
     if (date.isLessOrEqual('2013-09-16')) {
       outputResponse.statusCode = 400
@@ -16,7 +90,7 @@ export class CbrService {
       return outputResponse
     }
     if (date.isMoreOrEqual(dateToday)) date = dateToday
-    console.log('By ', date)
+    console.log('By ', date)*/
     let flag = false // checks whether cbr server sent a value
     await soap
       .createClientAsync(CbrService.wsdl)
