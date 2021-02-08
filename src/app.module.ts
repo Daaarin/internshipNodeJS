4b2010f52a5ab4b39fabb25f9ed2cbb7c2e29494
Asmolve <Asmolve@users.noreply.github.com>
2021-02-08 15:31:11 +0300

    There's only okpd

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,25 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiController } from './api/api.controller';
 import { ApiModule } from './api/api.module';
-import { CbrModule } from './api/cbr/cbr.module';
-import { CbrController } from './api/cbr/cbr.controller';
-import { CbrService } from './api/cbr/cbr.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OkpdModule } from './api/okpd/okpd.module';
 
 @Module({
   imports: [
     ApiModule,
-    CbrModule,
     OkpdModule,
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: './src/api/okpd/database/sqlite/okpd.db',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true
     })
   ],
-  controllers: [AppController, ApiController, CbrController],
-  providers: [AppService, CbrService]
+  controllers: [AppController, ApiController],
+  providers: [AppService]
 })
 export class AppModule {}
