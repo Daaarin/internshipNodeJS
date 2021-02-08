0d1b03a6926e1d5a4f6b6df96e36576c50772115
Asmolve <Asmolve@users.noreply.github.com>
2021-02-08 16:17:01 +0300

    Removed unnecessary import

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppService } from '../app.service';
 import { CbrModule } from './cbr/cbr.module';
 import { ApiService } from './api.service';
 import { OkpdModule } from './okpd/okpd.module';
 
 @Module({
   imports: [CbrModule, OkpdModule],
-  providers: [ApiService],
+  providers: [ApiService]
 })
 export class ApiModule {}
 
